0eeca286c86b8e897111484f6afa1889ed779362
Daniel Skopalik <d.skopalik@gmail.com>
2023-12-04 07:58:01 +0100

    cleanup main folder

--- PoCandTests/motionDetectionTest.py
+++ PoCandTests/motionDetectionTest.py
@@ -29,7 +29,26 @@ fgbg = cv2.createBackgroundSubtractorKNN()
 
 #cap = cv2.VideoCapture(0)
 
+def DetectNigthVision(image):
+    ### splitting b,g,r channels
+    b,g,r=cv2.split(image)
 
+    ### getting differences between (b,g), (r,g), (b,r) channel pixels
+    r_g=np.count_nonzero(abs(r-g))
+    r_b=np.count_nonzero(abs(r-b))
+    g_b=np.count_nonzero(abs(g-b))
+
+    ### sum of differences
+    diff_sum=float(r_g+r_b+g_b)
+
+    ### finding ratio of diff_sum with respect to size of image
+    ratio=diff_sum/image.size
+
+    print("ratio: ", ratio)
+    if ratio>0.1:
+        print("image is color")
+    else:
+        print("image is greyscale")
 
 startT = time.time()
 frameCount = 0
@@ -40,10 +59,8 @@ while (1):
         print("Frame is empty wait for 5s")
         time.sleep(5)
     else:
-
         frameCount += 1
-        if (frameCount % 5 > 0):
-            continue
+
         #convert to gray, even night vision images to save processing time
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.GaussianBlur(gray, (7, 7), 0) 
@@ -52,20 +69,17 @@ while (1):
     # black out polygon area
         pts = np.array([[0, 0], [0, 360], [80, 360], [150, 18] , [227, 18], [227, 0]], np.int32)
         cv2.fillPoly(gray, [pts], (0))
-        #cv2.imshow('gray', gray)
+        cv2.imshow('gray', gray)
     #Apply MOG 
         motion_mask = fgbg.apply(gray, LEARNING_RATE)
-        #cv2.imshow('motionMask', motion_mask)
+        cv2.imshow('motionMask', motion_mask)
         thresh_frame = cv2.threshold(motion_mask, 30, 255, cv2.THRESH_BINARY)[1] 
-        #cv2.imshow('thresh_frame', thresh_frame)
-        #cv2.waitKey(1)
+        cv2.imshow('thresh_frame', thresh_frame)
+        cv2.waitKey(1)
         nonZero = np.count_nonzero(thresh_frame)
-        if (nonZero > 500):
-            print("nonZero: ", nonZero)
-            cv2.imshow('motion detected', frame)
-            cv2.waitKey(1)
-       # time.sleep(0.3)
-    if (frameCount % 100 == 0):
+        print("nonZero: ", nonZero)
+        time.sleep(0.3)
+    if (frameCount % 20 == 0):
         ensT = time.time()
         print("FPS: ", frameCount/(ensT - startT))
         frameCount = 0
